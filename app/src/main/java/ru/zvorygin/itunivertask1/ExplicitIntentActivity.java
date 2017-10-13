610cda0ef3c40bd87ac359249861f3f8903ef28a
Dmitry Zvorygin <yettyzzzone@gmail.com>
2017-10-13 14:48:46 +0400

    Add explicit and implicit Intents. Create ExplicitIntentActivity.

---
+++ app/src/main/java/ru/zvorygin/itunivertask1/ExplicitIntentActivity.java
@@ -0,0 +1,13 @@
+package ru.zvorygin.itunivertask1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExplicitIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explicit_intent);
+    }
+}
--- app/src/main/java/ru/zvorygin/itunivertask1/MainActivity.java
+++ app/src/main/java/ru/zvorygin/itunivertask1/MainActivity.java
@@ -1,8 +1,13 @@
 package ru.zvorygin.itunivertask1;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +33,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/YettyZzZone/ItUniverTask1"));
+                startActivity(intent);
             }
         });
         fab.setImageResource(R.drawable.ic_github);
@@ -82,9 +89,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_explicit_intent) {
-            // Handle the camera action
+            Intent intent = new Intent(this, ExplicitIntentActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_implicit_intent) {
-
+            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:sales@65apps.com"));
+            startActivity(intent);
         } else if (id == R.id.nav_implicit_intent_intent_filter) {
 
         } else if (id == R.id.nav_fragment) {
